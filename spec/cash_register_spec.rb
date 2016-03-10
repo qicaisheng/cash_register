801fde8c27db967522a85c3e0742e2cf82150888
qicaisheng <qicaisheng@qq.com>
2016-03-11 02:40:07 +0800

    Add 95% discount function test

--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -24,5 +24,22 @@ describe CashRegister do
         ].join("\n")
       end
     end
+
+    context 'when have 95% discount' do
+      it 'puts with discount' do
+        Promotion.refresh(nine_five_discount: ['ITEM000003'])
+        CashRegister.perfom
+        expect($stdout.string).to eq [
+          '***<没钱赚商店>购物清单***',
+          '名称：羽毛球，数量：5个，单价：1.00(元)，小计：5.00(元)',
+          '名称：苹果，数量：2斤，单价：5.50(元)，小计：10.45(元)，节省0.55(元)',
+          '名称：可口可乐，数量：3瓶，单价：3.00(元)，小计：9.00(元)',
+          '----------------------',
+          '总计：24.45(元)',
+          '节省：0.55(元)',
+          "**********************\n"
+        ].join("\n")
+      end
+    end
   end
 end
