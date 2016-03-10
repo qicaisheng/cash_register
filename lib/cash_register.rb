7dc27dc667e0b0c35087d30ddc9aef8582fe65f9
qicaisheng <qicaisheng@qq.com>
2016-03-11 02:16:05 +0800

    Add skeleton for cash register

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,28 @@
 module CashRegister
   class << self
     def perfom
+      CommodityRecord.refresh
+      scan_shopping_commodities
+      print_shopping_list
+    end
+
+    private
+
+    def scan_shopping_commodities
+    end
+
+    def print_shopping_list
+      puts '***<没钱赚商店>购物清单***'
+      print_shopping_items
+      puts '----------------------'
+      print_price
+      puts '**********************'
+    end
+
+    def print_shopping_items
+    end
+
+    def print_price
     end
   end
 end
