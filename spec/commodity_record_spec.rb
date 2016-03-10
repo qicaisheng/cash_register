aae2307127371d82287e68ff8c32a0e53cd90c7d
qicaisheng <qicaisheng@qq.com>
2016-03-11 02:07:31 +0800

    Add commodity record find test

--- spec/commodity_record_spec.rb
+++ spec/commodity_record_spec.rb
@@ -1,10 +1,28 @@
 require 'commodity_record'
 
 describe CommodityRecord do
+  before do
+    CommodityRecord.refresh
+  end
+
   describe 'records' do
     it 'returns the commodity records' do
-      CommodityRecord.refresh
       expect(CommodityRecord.records.map(&:name)).to eq %w(羽毛球 可口可乐 苹果)
+    end
+  end
+
+  describe '#find' do
+    context 'when commodity in records' do
+      it 'returns the commodity' do
+        expect(CommodityRecord.find('ITEM000003').name).to eq '苹果'
+      end
+    end
+
+    context 'when commodity does not in records' do
+      it 'returns nil' do
+        egg = Commodity.new(name: '鸡蛋', unit: '斤', unit_price: 5, barcode: 'ITEM000004')
+        expect(CommodityRecord.find(egg.barcode)).to eq nil
+      end
     end
   end
 end
