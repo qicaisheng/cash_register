25bc057f0220046f35cbadc7d90eb535972773c6
qicaisheng <qicaisheng@qq.com>
2016-03-11 02:28:57 +0800

    Add scan shopping commodities function and print shopping list

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,10 @@
+require 'commodity_record'
+require 'shopping_item'
+
 module CashRegister
   class << self
+    attr_reader :shopping_list
+
     def perfom
       CommodityRecord.refresh
       scan_shopping_commodities
@@ -9,6 +14,19 @@ module CashRegister
     private
 
     def scan_shopping_commodities
+      file_path = File.expand_path('../../data/shopping_list.json', __FILE__)
+      barcode_array = JSON.parse(File.read(file_path))
+      barcode_with_quantity = barcode_array.group_by { |item| item }
+      @shopping_list = barcode_with_quantity.map do |k, v|
+        if k.include? '-'
+          barcode, quantity = k.split('-')
+        else
+          barcode = k
+          quantity = v.count
+        end
+        commodity = CommodityRecord.find(barcode)
+        ShoppingItem.new(commodity, quantity.to_i)
+      end
     end
 
     def print_shopping_list
@@ -20,9 +38,15 @@ module CashRegister
     end
 
     def print_shopping_items
+      @shopping_list.each(&:print_item)
     end
 
     def print_price
+      puts "总计：#{format('%.2f', cost_price)}(元)"
+    end
+
+    def cost_price
+      @shopping_list.map(&:price).reduce(:+)
     end
   end
 end
