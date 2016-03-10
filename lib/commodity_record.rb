5c7037e2a6a96498e4b3d419cba286210de750d0
qicaisheng <qicaisheng@qq.com>
2016-03-11 02:05:20 +0800

    Add commodity records and turn test to green

---
+++ lib/commodity_record.rb
@@ -0,0 +1,15 @@
+require 'json'
+require 'commodity'
+
+module CommodityRecord
+  class << self
+    attr_reader :records
+
+    def refresh
+      file_path = File.expand_path('../../data/commodity_records.json', __FILE__)
+      @records = JSON.parse(File.read(file_path)).map { |commodity_params|
+        Commodity.new(commodity_params)
+      }
+    end
+  end
+end
