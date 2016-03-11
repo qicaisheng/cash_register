ad7a18ca9e4f22f90c306d60fe86052978a080e1
qicaisheng <qicaisheng@qq.com>
2016-03-11 23:39:46 +0800

    Add test for both discounts

--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -62,5 +62,42 @@ describe CashRegister do
         ].join("\n")
       end
     end
+
+    context 'when have free one discount and nine five discount' do
+      it 'puts with discount' do
+        Promotion.refresh(
+          free_one: %w(ITEM000001 ITEM000005),
+          nine_five: ['ITEM000003']
+        )
+        CashRegister.perfom(
+          [
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000003-2',
+            'ITEM000005',
+            'ITEM000005',
+            'ITEM000005'
+          ]
+        )
+        expect($stdout.string).to eq [
+          '***<没钱赚商店>购物清单***',
+          '名称：羽毛球，数量：6个，单价：1.00(元)，小计：4.00(元)',
+          '名称：苹果，数量：2斤，单价：5.50(元)，小计：10.45(元)，节省0.55(元)',
+          '名称：可口可乐，数量：3瓶，单价：3.00(元)，小计：6.00(元)',
+          '----------------------',
+          '买二赠一商品：',
+          '名称：羽毛球，数量：2个',
+          '名称：可口可乐，数量：1瓶',
+          '----------------------',
+          '总计：20.45(元)',
+          '节省：5.55(元)',
+          "**********************\n"
+        ].join("\n")
+      end
+    end
   end
 end
